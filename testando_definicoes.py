67843106b8faf01a8bdb9fce70574e8826a8951a
Pedro Henrique Lopes Da Silva <p.lopes.judoca@gmail.com>
2025-05-26 15:24:18 -0400

    Add files via upload

--- testando_definicoes.py
+++ testando_definicoes.py
@@ -1,21 +1,32 @@
-compras = ["pao", "cafe", "leite"]
-print(compras)
+# compras = ["pao", "cafe", "leite"]
+# print(compras)
 
-#Pode ser removido pelo nome do produto ou pelo indice
-# compras.remove("cafe")
-compras.remove(compras[1])
-print(compras)
+# #Pode ser removido pelo nome do produto ou pelo indice
+# # compras.remove("cafe")
+# compras.remove(compras[1])
+# print(compras)
 
-#Append acrescenta apenas um item por vez na lista em seu final, de cada vez
-compras.append("sal")
-print(compras)
+# #Append acrescenta apenas um item por vez na lista em seu final, de cada vez
+# compras.append("sal")
+# print(compras)
 
-#é preciso criar uma lista nova para receber os elementos ordenados da lista antiga
-compras_ordenadas = sorted(compras)
-print(compras_ordenadas)
+# #é preciso criar uma lista nova para receber os elementos ordenados da lista antiga
+# compras_ordenadas = sorted(compras)
+# print(compras_ordenadas)
 
-#o nome item é um identificado dos itens detro da lista, esses itens podem receber qualquer tipo de nome
-for item in compras:
-    print("-", item)
+# #o nome item é um identificado dos itens detro da lista, esses itens podem receber qualquer tipo de nome
+# for item in compras:
+#     print("-", item)
+#     # listas[]
+#     #dicionarios {chave-valor}
+#     #tuplas= ()
+#     #conjuntos= set()
     
-    
+# teste=[1, 2, 3, 5]
+# print(teste)
+# teste.remove(1)
+# print(teste)
+teste = {1, 2, 3, 5}
+print(teste)
+teste.remove(teste(1))
+print(teste)
